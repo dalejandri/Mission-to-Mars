c166d643d914acdd45f75b1d788760804813d240
dalejandri <86029441+dalejandri@users.noreply.github.com>
2021-08-30 01:03:08 -0500

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -9,7 +9,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
+    browser = Browser('chrome', **executable_path, headless=True)
 
     news_title, news_paragraph = mars_news(browser)
 
@@ -19,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemisphere_img(browser)
     }
 
     # Stop webdriver and return data
@@ -31,7 +32,7 @@ def mars_news(browser):
 
     # Scrape Mars News
     # Visit the mars nasa news site
-    url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
+    url = 'https://redplanetscience.com/'
     browser.visit(url)
 
     # Optional delay for loading the page
@@ -57,7 +58,7 @@ def mars_news(browser):
 
 def featured_image(browser):
     # Visit URL
-    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    url = 'https://spaceimages-mars.com'
     browser.visit(url)
 
     # Find and click the full image button
@@ -77,7 +78,7 @@ def featured_image(browser):
         return None
 
     # Use the base url to create an absolute url
-    img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 
     return img_url
 
@@ -85,7 +86,7 @@ def mars_facts():
     # Add try/except for error handling
     try:
         # Use 'read_html' to scrape the facts table into a dataframe
-        df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
 
     except BaseException:
         return None
@@ -96,6 +97,72 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def hemisphere_img(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.container', wait_time=1)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+    title_lst = []
+    url_lst = []
+    urlFull_img_lst = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    # Parse the HTML
+    html = browser.html
+    html_soup = soup(html, 'html.parser')
+
+    # Add try/except for error handling
+    try:
+        # Define the sections for scrape
+        section = html_soup.find('div', class_= 'collapsible results')
+        items = section.find_all('div', class_='item')
+
+        # ForLoop in each items
+        for item in items:
+            # Scrape the title
+            title = item.find('h3').text
+            title_lst.append(title)
+            
+            # Scrape the url for the detail page
+            url = f'https://marshemispheres.com/{item.find("a")["href"]}'
+            url_lst.append(url)
+            
+            # Go to the detail page - Use browser to visit the URL 
+            browser.visit(url)
+            
+            # Optional delay for loading the page
+            browser.is_element_present_by_css('div.container', wait_time=1)
+            
+            # Parse the HTML detail page
+            html = browser.html
+            html_soup = soup(html, 'html.parser')
+            
+            # Scrape the full image url
+            section = html_soup.find('div', class_='downloads')
+            url_full_img = f'https://marshemispheres.com/{section.find("a")["href"]}'
+            urlFull_img_lst.append(url_full_img)
+
+        # Add dictionary
+        for url,title in zip(urlFull_img_lst,title_lst):
+            # Create the dictionary
+            dic = {
+                "img_url": url,
+                "title": title
+            }
+            # Add the objet to the list
+            hemisphere_image_urls.append(dic)
+    
+    except AttributeError:
+        return None
+    
+    # 4. Return the list that holds the dictionary of each image url and title.
+    return hemisphere_image_urls
+
 
 if __name__ == "__main__":
 
